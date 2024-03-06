e95fcbffcfa77a75e3ab54ae5c40e5424462e31a
NAM2412 <20520643@gm.uit.edu.vn>
2024-03-06 21:12:34 +0700

    feat (JoyStick): moves the joystick with touch input

--- Assets/Scripts/JoyStick.cs
+++ Assets/Scripts/JoyStick.cs
@@ -5,19 +5,29 @@ using UnityEngine.EventSystems;
 
 public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] RectTransform thumbStickTrans;
+    [SerializeField] RectTransform backgroundTrans;
+    [SerializeField] RectTransform centerTrans;
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log($"On Drag: {eventData.position}");
+        Vector2 touchPos = eventData.position;
+        Vector2 centerPos = backgroundTrans.position;
+
+        Vector2 loccalOffset = Vector2.ClampMagnitude(touchPos - centerPos, backgroundTrans.sizeDelta.x/4);
+        thumbStickTrans.position = centerPos + loccalOffset;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Down!");
+        backgroundTrans.position = eventData.position;
+        thumbStickTrans.position = eventData.position;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Up!");
+        backgroundTrans.position = centerTrans.position;
+        thumbStickTrans.position = backgroundTrans.position;
     }
 
     // Start is called before the first frame update
