4b659e60fa7dceb799b627473b0c0bf325df5abf
NAM2412 <20520643@gm.uit.edu.vn>
2024-03-09 21:52:20 +0700

    style: recfactor Player scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,32 +30,10 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-        Vector3 moveDir = StickInputToWorldDirection(moveInput);
-        Vector3 aimDir = moveDir;
-
-        if (aimInput.magnitude != 0) 
-        {
-            aimDir = StickInputToWorldDirection(aimInput);
-        }
-
-        if (aimDir.magnitude != 0)
-        {
-            float turnLerpAlpha = turnSpeed * Time.deltaTime;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(aimDir, Vector3.up), turnLerpAlpha);
-        }
-
-        characterController.Move(moveDir * Time.deltaTime * moveSpeed);
-
-        if (moveInput.magnitude != 0) // player is moving
-        { 
-            if (cameraController != null)
-            {
-                cameraController.AddYawInput(moveInput.x);
-            }
-        }
+        MoveAndAim();
+        UpdateCamera();
     }
     #endregion
-
     #region OnStickValueUpdate Event
     private void AimStick_OnStickValueUpdated(Vector2 inputValue)
     {
@@ -67,8 +45,43 @@ public class Player : MonoBehaviour
         moveInput = inputValue;
     }
     #endregion
+    private void MoveAndAim()
+    {
+        Vector3 moveDir = StickInputToWorldDirection(moveInput);
+        characterController.Move(moveDir * Time.deltaTime * moveSpeed);
 
-    Vector3 StickInputToWorldDirection (Vector2 inputValue)
+        UpdateAim(moveDir);
+    }
+
+    private void UpdateAim(Vector3 currentMoveDir)
+    {
+        Vector3 aimDir = currentMoveDir;
+        if (aimInput.magnitude != 0)
+        {
+            aimDir = StickInputToWorldDirection(aimInput);
+        }
+        RotateToward(aimDir);
+    }
+
+    private void UpdateCamera()
+    {
+        // Player is moving but not aiming
+        if (moveInput.magnitude != 0 && aimInput.magnitude == 0 && cameraController != null)
+        {
+            cameraController.AddYawInput(moveInput.x);
+        }
+    }
+
+    private void RotateToward(Vector3 aimDir)
+    {
+        if (aimDir.magnitude != 0)
+        {
+            float turnLerpAlpha = turnSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(aimDir, Vector3.up), turnLerpAlpha);
+        }
+    }
+
+    private Vector3 StickInputToWorldDirection (Vector2 inputValue)
     {
         Vector3 rightDir = mainCamera.transform.right;
         Vector3 upDir = Vector3.Cross(rightDir, Vector3.up);
