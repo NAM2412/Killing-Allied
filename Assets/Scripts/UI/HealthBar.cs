11bb0d029c976d7af72b58b262341d0587c923fe
NAM2412 <20520643@gm.uit.edu.vn>
2024-04-01 21:03:37 +0700

    feat (UI): create the health bar for enemy

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] Slider healthSlider;
+
+    private Transform _attachPoint;
+    public void Init(Transform attachPoint)
+    {
+        this._attachPoint = attachPoint;
+    }
+    public void SetHealthSliderValue(float val)
+    {
+        healthSlider.value = val;
+    }
+
+    private void Update()
+    {
+        Vector3 attachScreenPoint = Camera.main.WorldToScreenPoint(_attachPoint.position);
+        transform.position = attachScreenPoint;
+    }
+}
