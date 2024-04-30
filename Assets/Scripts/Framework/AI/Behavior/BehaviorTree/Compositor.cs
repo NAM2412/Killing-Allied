291669142a9033456e4ade8a6c988f488927ee44
NAM2412 <20520643@gm.uit.edu.vn>
2024-04-30 17:05:13 +0700

    feat (AI Behavior): create the compositor class

---
+++ Assets/Scripts/Framework/AI/Behavior/BehaviorTree/Compositor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Compositor : Node
+{
+    LinkedList<Node> children = new LinkedList<Node>();
+    LinkedListNode<Node> currentChild = null;
+
+    protected override NodeResult Execute()
+    {
+        if (children.Count == 0)
+        {
+            return NodeResult.Success;
+        }
+
+        currentChild = children.First;
+        return NodeResult.Inprogress;
+    }
+
+    protected override NodeResult Update()
+    {
+        return base.Update();
+    }
+
+    protected bool Next()
+    {
+        if(currentChild != children.Last)
+        {
+            currentChild = currentChild.Next;
+            return true;
+        }
+        return false;
+    }
+
+    protected override void End()
+    {
+        currentChild = null;
+    }
+}
