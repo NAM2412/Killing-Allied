f7deff2c905a397eeb455570c5da44842c617a0e
NAM2412 <20520643@gm.uit.edu.vn>
2024-05-09 23:15:17 +0700

    feat (AI Behaviour): create The Move To Target Task

--- Assets/Scripts/Framework/AI/Behavior/ChomperBehavior.cs
+++ Assets/Scripts/Framework/AI/Behavior/ChomperBehavior.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class ChomperBehavior : BehaviorTree
 {
     protected override void ConstructTree(out Node rootNode)
-    { 
-        Task_Wait waitTask = new Task_Wait(2f);
-        Task_Log log = new Task_Log("Logging");
+    {
+        Task_MoveToTarget moveToTarget = new Task_MoveToTarget(this, "Target", 2f);
 
-        Sequencer root = new Sequencer();
-        root.AddChild(log);
-        root.AddChild(waitTask);
-
-        rootNode = root;
+        rootNode = moveToTarget;
     }
 }
